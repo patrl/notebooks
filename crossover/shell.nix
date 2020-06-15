8adf29cba2b93f08637c2c80660c8d1f06ce4f73
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-15 18:10:39 -0400

    first commit

---
+++ crossover/shell.nix
@@ -0,0 +1,15 @@
+let
+  sources = import ./nix/sources.nix;
+  jupyter = import sources.jupyter {};
+
+  iHaskell = jupyter.kernels.iHaskellWith {
+    name = "haskell";
+    packages = p: with p; [ containers ];
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iHaskell ];
+    };
+in
+  jupyterEnvironment.env
