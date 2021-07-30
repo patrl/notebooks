35b573cc123198727e9be728f22630a2b639dd09
Patrick Elliott <patrick.d.elliott@gmail.com>
2021-07-30 13:48:53 +0100

    Added amr scratchpad

---
+++ amrs/shell.nix
@@ -0,0 +1,16 @@
+let
+  sources = import ./nix/sources.nix;
+  jupyter = import sources.jupyter {};
+
+  iHaskell = jupyter.kernels.iHaskellWith {
+    name = "haskell";
+    packages = p: with p; [ containers ];
+    extraIHaskellFlags = "--codemirror Haskell";
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iHaskell ];
+    };
+in
+  jupyterEnvironment.env
